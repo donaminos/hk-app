89d0d8acee3be928c6dcff840aa228610ff1c44d
donaminos <amine.sadry@gmail.com>
2014-08-08 12:08:03 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HkApp::Application.config.secret_key_base = '59cbb8e53982853874d61d209ff712858f9af56187de421dde8b5ac5f045da180f7ff2bc8ebb0f527f098aeb5aa85b58c3652e8c7e93a17ffa141b70a402ddcf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HkApp::Application.config.session_store :cookie_store, key: '_hk-app_session'
